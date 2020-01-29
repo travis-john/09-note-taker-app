78e0be6ba8b2e7194874f26ea9091651e71ca295
travis-john <travis.wl.john@gmail.com>
2020-01-29 12:56:58 -0500

    server and express routes are working

--- server.js
+++ server.js
@@ -12,10 +12,20 @@ const readFile = util.promisify(fs.readFile);
 const PORT = process.env.PORT || 3000;
 
 // CONFIGURING EXPRESS
-app.use(express.urlencoded({
-  extended: true;
-}));
-app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//DEFINING FROM WHERE STATIC FILES ARE SERVED
+app.use(express.static(path.join(__dirname, 'public')));
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 //STARTING SERVER
 app.listen(PORT, function() {
