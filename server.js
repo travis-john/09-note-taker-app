b123ebb2e6c8ded0155dcdb88e0b626e4a7407b1
travis-john <travis.wl.john@gmail.com>
2020-01-29 14:45:22 -0500

    writing new note to db.json

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // GLOBAL VARIABLES
-const express = require('express');
-const path = require('path');
-const db = require('./db/db.json');
-const fs = require('fs');
-const util = require('util');
-const app = express();
-const writeFile = util.promisify(fs.writeFile);
-const readFile = util.promisify(fs.readFile);
+const express = require('express'),
+      path = require('path'),
+      db = require('./db/db.json'),
+      fs = require('fs'),
+      util = require('util'),
+      app = express(),
+      writeFile = util.promisify(fs.writeFile),
+      readFile = util.promisify(fs.readFile);
 
 // DEFINING PORT AND ALLOWING FOR HEROKU'S PORT
 const PORT = process.env.PORT || 3000;
@@ -26,6 +26,41 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+//GETTING AND PARSING THE NOTES JSON FILE
+app.get('/api/notes', (req,res) => {
+  readFile('./db/db.json', 'utf-8').then(function(data){
+    // console.log(data);
+    data = JSON.parse(data);
+    return res.json(data);
+  });
+});
+
+//WRITING NEW NOTES TO DB.JSON
+app.post('/api/notes', (req, res) => {
+
+  let newNote = req.body;
+
+  //READING BEFORE ADDING NEW NOTE TO DB.JSON
+  readFile('./db/db.json', 'utf-8').then(function(data) {
+
+    //PARSING JSON
+    data = JSON.parse(data);
+
+    //PUSHING NEW NOTE TO DB.JSON
+    data.push(newNote);
+
+    //APPENDING THE NEW NOTE TO THE DB.JSON
+    data[data.length - 1].id = data.length - 1;
+
+    //WRITING NEW DB.JSON FILE AFTER THE NEW NOTE HAS BEEN ADDED TO THE ARRAY
+    writeFile('./db/db.json', JSON.stringify(data));
+  })
+
+  //CONFIRMING THAT NEW NOTE HAS BEEN CREATED
+  res.send('created new note');
+})
+
 
 //STARTING SERVER
 app.listen(PORT, function() {
